6a2974b79e3e0beb7dd8a8ed65af6b46a56c8254
Tomasz Lewinski <tomasz.lewinski@scalac.io>
2024-01-30 10:50:19 +0100

    working application

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-function App() {
+import { useState, useEffect } from "react";
+import { fetchUserAttributes } from "aws-amplify/auth";
+import { WithAuthenticatorProps } from "@aws-amplify/ui-react";
+
+function App(props: WithAuthenticatorProps) {
+  const [email, setEmail] = useState<string>("");
+  useEffect(() => {
+    fetchUserAttributes().then((userAttrs) => setEmail(userAttrs.email || ""));
+  }, []);
   return (
     <>
-      <p>Hello</p>
+      <p>Hello {email}</p>
+      <p>
+        You can <button onClick={props.signOut}>logout</button>
+      </p>
     </>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Amplify } from "aws-amplify";
+import { withAuthenticator } from "@aws-amplify/ui-react";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import "@aws-amplify/ui-react/styles.css";
+import App from "./App.tsx";
+
+import "./index.css";
+
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      loginWith: {
+        oauth: {
+          redirectSignIn: ["http://localhost:5173"],
+          redirectSignOut: ["http://localhost:5173"],
+          domain: "scalac-blogpost.auth.eu-central-1.amazoncognito.com",
+          providers: ["Google"],
+          scopes: ["email", "openid", "aws.cognito.signin.user.admin"],
+          responseType: "code",
+        },
+      },
+      userPoolId: "eu-central-1_IcVlatAcd",
+      userPoolClientId: "2sbh33a9n37q9of2vvtei1d1o",
+    },
+  },
+});
+
+const AppWithAuth = withAuthenticator(App);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <AppWithAuth />
   </React.StrictMode>,
-)
+);
